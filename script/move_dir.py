5a6ad8958a99fd31e947201d284e29ceaacdc640
Reza <101015213@qq.com>
2021-10-31 14:28:06 +0800

    Datasets script update

--- script/move_dir.py
+++ script/move_dir.py
@@ -1,17 +1,17 @@
 import os
 import shutil
 
+DATASETS_LENGTH = 149
 video_name = ["150", "200", "250", "300"]
-path = ".." + os.sep + ".." + os.sep + "Datasets" + os.sep + "Training"
+path = ".." + os.sep + ".." + os.sep + "RAW_DATASETS"
 if not os.path.exists(path):
     os.mkdir(path)
-
 for name in video_name:
     dst_path = os.path.join(path,name)
     if not os.path.exists(dst_path):
         os.mkdir(dst_path)
-    for i in range(8):
-        frame_path = ".." + os.sep + ".." + os.sep + "Datasets" + os.sep + "drone_view_cut_frame"
+    for i in range(DATASETS_LENGTH):
+        frame_path = ".." + os.sep + ".." + os.sep + "RAW_DATASETS" + os.sep + "drone_view_cut_frame"
         dir_name = "{:0>4d}".format(i + 1)
         frame_num_path = os.path.join(frame_path, dir_name)
         # print(frame_num_path)
