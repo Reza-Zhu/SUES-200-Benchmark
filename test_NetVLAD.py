f20cef3409ce05d6a29ae37c088fa2d6af86b1c6
Reza <101015213@qq.com>
2021-12-21 21:57:55 +0800

    NetVLAD V1.0

---
+++ test_NetVLAD.py
@@ -0,0 +1,92 @@
+import os
+import torch
+import numpy as np
+from utils import get_yaml_value, get_id, get_model_list
+from evaluate import compute_mAP, evaluate
+from Preprocessing import Create_Testing_Datasets
+from torchvision import models, datasets, transforms
+from NetVLAD.netvlad import NetVLAD, EmbedNet
+
+
+encoder = models.resnet18(pretrained=True)
+base_model = torch.nn.Sequential(
+    encoder.conv1,
+    encoder.bn1,
+    encoder.relu,
+    encoder.maxpool,
+    encoder.layer1,
+    encoder.layer2,
+    encoder.layer3,
+    encoder.layer4,
+)
+
+dim = list(base_model.parameters())[-1].shape[0]
+netVLAD = NetVLAD(num_clusters=89, dim=dim, alpha=1.0)
+model = EmbedNet(base_model, netVLAD).cuda()
+
+name = get_yaml_value("name")
+save_dirname = os.path.join("./save_model_weight", name)
+last_model_name = os.path.basename(get_model_list(save_dirname, 'net', -1))
+print(last_model_name)
+model.load_state_dict(torch.load(os.path.join(save_dirname, last_model_name)))
+
+query_name = 'query_drone'
+gallery_name = 'gallery_satellite'
+
+image_datasets, data_loader = Create_Testing_Datasets()
+query_path = image_datasets[query_name].imgs
+gallery_path = image_datasets[gallery_name].imgs
+
+gallery_label, gallery_path = get_id(gallery_path)
+query_label, query_path = get_id(query_path)
+
+query_label = np.array(query_label)
+gallery_label = np.array(gallery_label)
+
+query_feature = torch.FloatTensor()
+gallery_feature = torch.FloatTensor()
+
+print("<<<<<<<<<Testing Start>>>>>>>>>>>>")
+
+with torch.no_grad():
+    for img, label in data_loader[gallery_name]:
+        n, c, h, w = img.size()
+        output = model(img.cuda())
+        gallery_feature = torch.cat((gallery_feature, output.data.cpu()))
+    print(gallery_feature.size())
+    for img, label in data_loader[query_name]:
+        n, c, h, w = img.size()
+        output = model(img.cuda())
+        query_feature = torch.cat((query_feature, output.data.cpu()))
+    print(query_feature.size())
+
+
+print("<<<<<<<<<Evaluating Start>>>>>>>>>>>>")
+CMC = torch.IntTensor(len(gallery_label)).zero_()
+# ap = average precision
+ap = 0.0
+
+query_feature = query_feature.cuda()
+gallery_feature = gallery_feature.cuda()
+
+for i in range(len(query_label)):
+    # print(query_label[])
+    ap_tmp, CMC_tmp = evaluate(query_feature[i], query_label[i], gallery_feature, gallery_label)
+    # print(CMC_tmp.shape)
+    if CMC_tmp[0] == -1:
+        continue
+    CMC += CMC_tmp
+    ap += ap_tmp
+
+CMC = CMC.float()
+CMC = CMC / len(query_label)
+result = 'Recall@1:%.2f Recall@5:%.2f Recall@10:%.2f Recall@top10:%.2f AP:%.2f' % (
+    CMC[0] * 100, CMC[4] * 100, CMC[9] * 100, CMC[round(len(gallery_label) * 0.1)] * 100,
+    ap / len(query_label) * 100)
+
+save_path = os.path.join('save_model_weight', get_yaml_value('name'))
+save_txt_path = os.path.join(save_path,
+                             '%s_to_%s_%s_%.2f_%.2f.txt' % (query_name[6:], gallery_name[8:], last_model_name[:7],
+                                                            CMC[0] * 100, ap / len(query_label)))
+
+print(result)
---
+++ train_NetVLAD.py
@@ -0,0 +1,75 @@
+import os
+import time
+import torch
+import torch.nn as nn
+from torchvision import models
+from utils import get_yaml_value, save_network
+from torch.optim import lr_scheduler
+from NetVLAD.netvlad import NetVLAD, EmbedNet
+from NetVLAD.tripleloss import HardTripletLoss
+from Preprocessing import Create_Training_Datasets
+
+encoder = models.resnet18(pretrained=True)
+base_model = nn.Sequential(
+    encoder.conv1,
+    encoder.bn1,
+    encoder.relu,
+    encoder.maxpool,
+    encoder.layer1,
+    encoder.layer2,
+    encoder.layer3,
+    encoder.layer4,
+)
+
+num_epochs = get_yaml_value("num_epochs")
+height = get_yaml_value("height")
+classes = get_yaml_value("classes")
+model_name = get_yaml_value("model")
+
+# lr = get_yaml_value("lr")
+
+dim = list(base_model.parameters())[-1].shape[0]
+netVLAD = NetVLAD(num_clusters=classes, dim=dim, alpha=1.0)
+model = EmbedNet(base_model, netVLAD).cuda()
+
+criterion = HardTripletLoss(margin=0.1).cuda()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.05, weight_decay=1e-5, momentum=0.9, nesterov=True)
+scheduler = lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=5, T_mult=2)
+
+data_loader = Create_Training_Datasets()
+print("<<<<<<<<<Training Start>>>>>>>>>>>>")
+current_time = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
+
+min_loss = 0.0020
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    total = 0.0
+    model.train(True)
+    for batch_dix, (input1, label1) in enumerate(data_loader["drone_train"]):
+        input1 = input1.cuda()
+        label1 = label1.cuda()
+        total += label1.size(0)
+        optimizer.zero_grad()
+        output1 = model(input1)
+        loss = criterion(output1, label1)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+        # print('[ In Epoch {}-{}] {} | Loss: {:.8f} | '\
+        #       .format(epoch + 1, batch_dix, "Train", loss.item()))
+    scheduler.step()
+    epoch_loss = running_loss/total
+    print('<<<<[Epoch {}/{}] {} | Loss: {:.8f} |>>>>'\
+          .format(epoch + 1, num_epochs, "Train", epoch_loss))
+    save_dir = "./save_model_weight/NetVLAD_%s_%s" % (height, current_time)
+
+    if epoch_loss < min_loss:
+        min_loss = epoch_loss
+        save_network(model, model_name, current_time, epoch + 1)
+        print(model_name + "Epoch: " + str(epoch + 1) + " has saved with loss: " + str(epoch_loss))
+
+        # if not os.path.exists(save_dir):
+        #     os.mkdir(save_dir)
+        # torch.save(model.state_dict(),
+        #            os.path.join(save_dir, "net_%03d.pth" % epoch))
